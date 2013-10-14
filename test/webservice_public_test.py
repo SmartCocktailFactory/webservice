add06f748f44d5d94e436d93b7dd29742b97570f
rolandabt <roland.abt@gmx.net>
2013-10-14 17:41:27 +0200

    added more tests

--- test/webservice_public_test.py
+++ test/webservice_public_test.py
@@ -1,33 +1,49 @@
-import os
 import webservice
 import unittest
-import tempfile
 import json
 
 class WebservicePublicTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, webservice.app.config['DATABASE'] = tempfile.mkstemp()
         webservice.app.config['TESTING'] = True
         self.app = webservice.app.test_client()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(webservice.app.config['DATABASE'])
+        pass #empty
 
-    def get_response(self, uri):
+    def get(self, uri):
         response = self.app.get(uri)
+        jsonResponse = json.loads(response.data)
+        return jsonResponse
+
+    def put(self, uri):
+        response = self.app.put(uri)
         jsonResponse = json.loads(response.data)
         return jsonResponse
 
     def test_getIndex(self):
         expected = 'Welcome to the Smart Cocktail Factory'
-        self.assertEqual(expected, self.get_response('/'))
+        self.assertEqual(expected, self.get('/'))
 
     def test_getDrinks(self):
-        response = self.get_response('/drinks')
+        response = self.get('/drinks')
         self.assertEqual(list, type(response))
         self.assertEqual(4, len(response))
+
+    def test_orderDrink(self):
+        response = self.put('/orders/Martini')
+        self.assertEquals(int, type(response))
+        response_next = self.put('/orders/Martini')
+        self.assertEquals(response_next, response + 1)
+
+    def test_orderDrinkThenCheckOrders(self):
+        #given
+        ordersResponseOld = self.get('/orders')
+        # when
+        self.put('/orders/Martini')
+        ordersResponseNew = self.get('/orders')
+        #then
+        self.assertEquals(len(ordersResponseOld) + 1, len(ordersResponseNew))
 
 if __name__ == '__main__':
     unittest.main()
